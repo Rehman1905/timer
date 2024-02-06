a0886960bf1ac423e8552b79dbd3ca6e3db19f5d
Rehman1905 <ll.mmdoff.ll@gmail.com>
2024-02-06 19:37:28 +0400

    timer

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-let reqem =0;
+let reqem = 0;
 let t;
 let saniye;
 let deqiqe;
 let saat;
-function timer () {
+let a = false; 
+
+function timer() {
     reqem++;
-    saat=`0${Math.floor(reqem/3600)}`.slice(-2)
-    deqiqe=`0${Math.floor((reqem-saat*3600)/60)}`.slice(-2)
-    saniye=`0${reqem%60}`.slice(-2)
+    saat = `0${Math.floor(reqem / 3600)}`.slice(-2)
+    deqiqe = `0${Math.floor((reqem - saat * 3600) / 60)}`.slice(-2)
+    saniye = `0${reqem % 60}`.slice(-2)
     document.querySelector('#timer').innerHTML = `${saat}:${deqiqe}:${saniye}`;
 }
-let a=false
-function start () {
+
+function start() {
     if (!t) {
         t = setInterval(timer, 1000);
+        a = true; 
     }
-    a=true
 }
 
-function stop () {
+function stop() {
     clearInterval(t);
     t = undefined;
-}
-function reset(){
     a=false
-    reqem=0
-    document.querySelector('#timer').innerHTML = `00:00:00`
 }
 
-function lap(){
-    if(a){
-        const dovr=document.querySelector('#dovr')
-        const newDiv=document.createElement('div')
+function reset() {
+    document.querySelector('#timer').innerHTML = `00:00:00`;
+    document.querySelector('#dovr').innerHTML = ''
+    reqem = 0;
+}
+
+function lap() {
+    if (a) { 
+        const dovr = document.querySelector('#dovr')
+        const newDiv = document.createElement('div')
         newDiv.classList.add('lap-div')
         dovr.append(newDiv)
-        newDiv.innerHTML=document.querySelector('#timer').innerHTML = `${saat}:${deqiqe}:${saniye}`
-    }else{
-        alert('press the start button')
+        newDiv.innerHTML = `${saat}:${deqiqe}:${saniye}`
+    } else {
+        alert('Press the start button');
     }
 }
 
-function clear(){
-    dovr.innerHTML=''
-}
+
+
 document.querySelector('#start').addEventListener('click', start);
 document.querySelector('#stop').addEventListener('click', stop);
-document.querySelector('#reset').addEventListener('click',reset)
-document.querySelector('#lap').addEventListener('click',lap)
-document.querySelector('#clear').addEventListener('click',clear)
+document.querySelector('#reset').addEventListener('click', reset);
+document.querySelector('#lap').addEventListener('click', lap);
